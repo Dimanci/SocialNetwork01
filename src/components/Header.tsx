f52603fb79606283854f469cd5cb3702a8bfbd7a
Dzmitry Kazakou <dimancika@gmail.com>
2022-12-06 21:00:27 -0500

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header =()=> {
+    return (
+        <header className='header'>
+            <img id='logo' src={'https://i.pinimg.com/originals/0a/41/0e/0a410e3a4f03610eed0dbbbcd2f0a3db.png'}/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img id='hogwarts' src='http://i.imgur.com/CMWTqJx.jpg?1'/></div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Posts
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
