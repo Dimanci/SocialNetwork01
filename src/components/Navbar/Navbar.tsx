df40eea201ae5a6bf386a9348cad16b0825b1b75
Dzmitry Kazakou <dimancika@gmail.com>
2022-12-18 22:36:20 -0500

    Working with BrowserRouter

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Navbar.module.css';
+
+const Navbar =()=> {
+    return (
+        <nav className={s.nav}>
+            <div className = {s.item}><a href={'/Profile'}>Profile</a></div>
+            <div className = {s.item}><a href={'/Dialogs'}>Messages</a></div>
+            <div className = {s.item}><a href={'/News'}>News</a></div>
+            <div className = {s.item}><a href={'/Music'}>Music</a></div>
+            <div className = {s.item}><a href={'/Settings'}>Settings</a></div>
+        </nav>
+    )
+}
+
+export default Navbar;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import s from './Profile.module.css'
-import MyPosts from "./My Posts/MyPosts";
+import MyPosts from "../My Posts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div><img id={s.hogwarts} src='http://i.imgur.com/CMWTqJx.jpg?1'/></div>
             <div>
                 Ava + description
