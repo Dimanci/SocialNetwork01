9e496ba5819c47c137eda4e48bfe82b86377c97d
Dzmitry Kazakou <dimancika@gmail.com>
2022-12-18 23:42:55 -0500

    Dialogs CSS

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.alldialogs}>
+            <div className={s.dialogs}>
+                <div className={s.dialogs}>Dima</div>
+                <div className={s.dialogs}>Katya</div>
+                <div className={s.dialogs}>Maksim</div>
+                <div className={s.dialogs}>Olivia</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialogs}>Hello</div>
+                <div className={s.dialogs}>What is going on!</div>
+                <div className={s.dialogs}>What time is it now?</div>
+                <div className={s.dialogs}>New movie was released.</div>
+            </div>
         </div>
     );
 };
